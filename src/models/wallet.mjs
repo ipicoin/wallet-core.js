e1a7920b585bf5038ad3dea79a9bf8598ceaed57
Sett Sarverott <sett@sarverott.com>
2026-05-21 19:02:30 +0200

    js ext to mjs for modules of ecma, model class inits, wallet receives some structures

---
+++ src/models/wallet.mjs
@@ -0,0 +1,16 @@
+import fs from "node:fs"
+import { URL } from "node:url"
+import path from "node:path"
+
+class Wallet{
+    get keyfile(){
+
+        return 
+    }
+    set keyfile(){
+
+    }
+    constructor(){
+
+    }
+}
