ca9645eba0a511411619cfa876648e8cbe0055c3
Sett Sarverott <sett@sarverott.com>
2026-06-29 16:32:03 +0200

    adding interchainjs, fixing vulnerable dependencies, signing controller removed for now because of been unsure if it wwill be directly in wallet model or in that controll unit

--- src/controllers/_index.mjs
+++ src/controllers/_index.mjs
@@ -2,11 +2,9 @@ import BasicController from "./_controller.mjs";
 import ChainController from "./chain-controller.mjs";
 import NetclientController from "./netclient-controller.mjs";
 import ConfigController from "./config-controller.mjs";
-import SigningController from "./signing-controller.mjs";
 
 export default Object.assign(BasicController, {
 	ChainController,
 	NetclientController,
 	ConfigController,
-	SigningController,
 });
--- src/controllers/config-controller.mjs
+++ src/controllers/config-controller.mjs
@@ -1,5 +1,15 @@
+/*
+  wallet-core - Copyright 2026 Sett Sarverott A.A.B. <sett@sarverott.com>
+*/
+
+import { cosmiconfigSync } from "cosmiconfig";
+
 import Controller from "./_controller.mjs";
 
-class ConfigController extends Controller {}
+class ConfigController extends Controller {
+	loadConfigData() {
+		return cosmiconfigSync("wallet-core").search();
+	}
+}
 
 export default ConfigController;
